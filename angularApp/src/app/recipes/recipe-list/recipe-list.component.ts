69baeeedb4d412d748378230af559e82bebd55e6
Kamil Sałęga <kamilsalega@gmail.com>
2017-10-14 21:53:10 +0200

    Wypisywanie listy receptur z ngFor

--- angularApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angularApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -10,7 +10,10 @@ import {Recipe} from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('Testowy Przepis', 'To jest tylko test przepisu', 'https://c.pxhere.com/photos/2a/26/italian_food_cuisine_restaurant_italian_food_meal_healthy_dinner-1195677.jpg!d')
+    new Recipe('Testowy Przepis', 'To jest tylko test przepisu',
+      'https://c.pxhere.com/photos/2a/26/italian_food_cuisine_restaurant_italian_food_meal_healthy_dinner-1195677.jpg!d'),
+    new Recipe('Testowy Przepis', 'To jest tylko test przepisu',
+      'https://c.pxhere.com/photos/2a/26/italian_food_cuisine_restaurant_italian_food_meal_healthy_dinner-1195677.jpg!d')
   ];
 
 
